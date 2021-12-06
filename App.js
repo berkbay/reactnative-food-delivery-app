563095bc22f76827dc41beba7ede5a4d567db62e
Berk Bay <berk.bay.ce@gmail.com>
2021-12-06 21:13:32 +0300

    Added basic Navigator code

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-// Berk Bay
+
 
 export default function App() {
   return (
